997e2ba56abc44def74e6488f5bf19ac5c5ac6fc
LuzIsabellaPerez <44623777+LuzIsabellaPerez@users.noreply.github.com>
2019-06-18 13:33:34 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,20 @@
+$(".bdk").hide();
+
+$("button").click(function(){
+    var rick = $(".de").val();
+        $(".uh").html(rick);
+        $(".uh").html(parseInt(rick));
+    var the = $(".la").val();
+        $(".uh").html(the);
+        $(".uh").html(parseInt(the));
+    var ruler = $(".soul").val();
+    var the = rick * the;
+    $(".uh").html("You will see Big Daddy Kane with " + ruler + " in Hamburg in " + the + " years");
+    $("body").css("background-image","url(https://cps-static.rovicorp.com/3/JPG_500/MI0000/314/MI0000314691.jpg?partner=allrovi.com)");
+    $(".bdk").show();
+    $(".jaMann").css("background-color","yellow");
+    $(".jaMann").css("padding","20px");
+    $(".jaMann").css("border","solid black 10px");
+    $(".jaMann").css("color","black");
+    $(".allesAberUh").hide();
+});
